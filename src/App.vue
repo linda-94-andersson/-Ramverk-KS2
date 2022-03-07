a20c4949ac24391c541df409b304fd3115ec0c8d
Linda A <linda.29.andersson@gmail.com>
2022-03-07 13:05:54 +0100

    Structure of Vue 2 and translation to Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script setup>
-import ToDoList from './components/ToDoList.vue';
+import ToDoList from "./components/ToDoList.vue";
+import ToDoListApp from "./components/To-Do-List-App.vue";
 </script>
 
 <template>
-  <ToDoList />
+  <!-- <ToDoList /> -->
+  <ToDoListApp />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/To-Do-List-App.vue
@@ -0,0 +1,68 @@
+<script module>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const newItem = ref("");
+    const tasks = ref([]);
+
+    function storeItem() {
+     tasks.value.push({
+         id: Date.now(),
+         done: false, 
+         content: newItem.value, 
+     });
+     newItem.value = ''; 
+    }
+    return {
+      newItem,
+      tasks,
+      storeItem,
+    };
+  },
+};
+
+//     delItem: function (task) {
+//       this.tasks.splice(this.tasks.indexOf(task), 1);
+//     },
+//   },
+// });
+</script>
+
+<template>
+  <div class="app-container" id="tasklist">
+    <h1 class="app-header">Vue TO DO LIST</h1>
+
+    <div class="add-task">
+      <input
+        type="text"
+        autocomplete="off"
+        placeholder="Add New Task"
+        class="task-input"
+        v-model="newItem"
+        @keyup.enter="storeItem"
+      />
+      <input
+        type="submit"
+        value="."
+        class="submit-task"
+        title="Add Task"
+        @click="storeItem"
+      />
+    </div>
+
+    <ul class="task-list">
+      <li class="task-list-item" v-for="task in tasks" :key="task.id"> 
+        <label class="task-list-item-label">
+          <input type="checkbox" />
+          <span>{{ task.content }}</span>
+        </label>
+        <span class="delete-btn" title="Delete Task" @click="delItem(task)">{{
+          task.del
+        }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
